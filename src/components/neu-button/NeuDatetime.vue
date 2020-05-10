826531900d21cc746fe2857f00c2deeee47b49d1
Matteo <orsini.matteo99@gmail.com>
2020-05-10 15:21:07 +0200

    Added NeuDatetime

---
+++ src/components/neu-button/NeuDatetime.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NeuInput type="date" class="mb-1" />
+    <NeuInput type="time" class="mt-1" />
+  </div>
+</template>
+
+<script>
+import NeuInput from "@/components/neu-button/NeuInput";
+
+export default {
+  name: "NueDatetime",
+  components: { NeuInput }
+};
+</script>
+
+<style></style>
